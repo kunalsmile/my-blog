1dd7a9faeb5b9cf583dc106bbbb8477629d17339
KGoel <kgoel@nextgen.com>
2019-07-29 21:12:16 +0530

    First commit for blog application

---
+++ src/build/precache-manifest.1dad203cb034e070658f49d1f880503d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ca9a95ef647694f1346da5d03b1796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d9f5be7d8993c2126e",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "b7d9f5be7d8993c2126e",
+    "url": "/static/js/main.7e41a030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
